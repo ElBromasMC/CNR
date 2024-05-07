7730254bda7971a764d55335256e8b476983628a
Ander Linares <ander.linares@outlook.com>
2024-05-07 01:48:19 -0500

    wait

--- backend/db/init.js
+++ backend/db/init.js
@@ -14,53 +14,53 @@ db.authenticate()
     .then(() => console.log('Base de datos conectada'))
     .catch(error => console.log(error))
 
-// // Sync database
-// db.sync()
-//     .then(() => console.log('Base de datos reconfigurada'))
-//     .catch(error => console.log(error))
+// Sync database
+db.sync()
+    .then(() => console.log('Base de datos reconfigurada'))
+    .catch(error => console.log(error))
 
-// const adminGroup = new Group({
-//     name: "admin",
-//     description: "The administrator group"
-// });
-// await adminGroup.save();
+const adminGroup = new Group({
+    name: "admin",
+    description: "The administrator group"
+});
+await adminGroup.save();
 
-// const publicGroup = new Group({
-//     name: "public",
-//     description: "The public group"
-// });
-// await publicGroup.save();
+const publicGroup = new Group({
+    name: "public",
+    description: "The public group"
+});
+await publicGroup.save();
 
-// const publicOrganization = new Organization({
-//     name: "public",
-//     description: "Public organization"
-// });
-// await publicOrganization.save();
+const publicOrganization = new Organization({
+    name: "public",
+    description: "Public organization"
+});
+await publicOrganization.save();
 
-// const adminUser = new User({
-//     name: "Administrador",
-//     email: process.env.ADMIN_EMAIL,
-//     password: process.env.ADMIN_PASSWORD,
-//     token: null,
-//     confirmed: true,
-//     groupId: 1,
-// });
-// await adminUser.save();
+const adminUser = new User({
+    name: "Administrador",
+    email: process.env.ADMIN_EMAIL,
+    password: process.env.ADMIN_PASSWORD,
+    token: null,
+    confirmed: true,
+    groupId: 1,
+});
+await adminUser.save();
 
-// const desayuno = new Meal({
-//     name: "Desayuno"
-// });
-// await desayuno.save();
+const desayuno = new Meal({
+    name: "Desayuno"
+});
+await desayuno.save();
 
-// const almuerzo = new Meal({
-//     name: "Almuerzo"
-// });
-// await almuerzo.save();
+const almuerzo = new Meal({
+    name: "Almuerzo"
+});
+await almuerzo.save();
 
-// const cena = new Meal({
-//     name: "Cena"
-// });
-// await cena.save();
+const cena = new Meal({
+    name: "Cena"
+});
+await cena.save();
 
 const desayunoType = new Type({
     id: 7,
